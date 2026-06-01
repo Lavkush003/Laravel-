f5549b4065d66f5c6bcb14b7ef0b19ed2fd8be71
Lavkush Nishad <lovekushnishad003@gmail.com>
2026-06-01 22:43:57 +0530

    view Basics

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,3 +4,20 @@
 <a href="/about">About</a>
 
 <a href="/post">Post Page</a>
+
+{{5+2}}
+<br><br>
+{{"Hello World"}}
+<br><br>
+{{"<h1>Hello World</h1>"}}
+
+@php
+    $names=["Sanjay Singh", "John Abraham","Sahid Kapoor"]
+
+@endphp
+
+<ul>
+@foreach ($names as $n)
+<li>{{$n}}</li>
+@endforeach
+</ul>
