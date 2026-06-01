5ae33e063f9909f862d58d0f1d55df3e597d40f3
Lavkush Nishad <lovekushnishad003@gmail.com>
2026-06-01 19:22:10 +0530

    Route Basics in laravel

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,22 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+ Route::get('/post', function (){
+    return view('post');
+});
+
+// Route::view('/post', 'post');
+
+// Route::get('/post/firstpost', function(){
+//     return view('firstpost');
+//});
+
+//Route Parameters
+Route::get('/post/{id?}', function (?string $id = null) {
+    if ($id !== null) {
+        return "<h1>Post ID: " . $id . "</h1>";
+    } else {
+        return "<h1>No ID Found</h1>";
+    }
+})->where('id', '[a-zA-Z]*');
