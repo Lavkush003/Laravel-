59572acb1a8e4873b07256da2d6616508a4d45f6
Lavkush Nishad <lovekushnishad003@gmail.com>
2026-06-02 13:40:36 +0530

    Blade template basics

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,8 @@
 {{"<h1>Hello World</h1>"}}
 
 @php
-    $names=["Sanjay Singh", "John Abraham","Sahid Kapoor"]
+    $names=["Sanjay Singh", "John Abraham","Sahid Kapoor",
+    "Sanjay Singh", "John Abraham","Sahid Kapoor"];
 
 @endphp
 
@@ -20,4 +21,18 @@
 @foreach ($names as $n)
 <li>{{$n}}</li>
 @endforeach
+</ul>
+
+{{-- foreach loop --}}
+
+
+<ul>
+@foreach ($names as $n)
+@if ($loop->first)
+    <li style="color:red;">{{ $n }}</li>
+    @elseif($loop->odd)
+     <li style="color:green;">{{$n}}</li>
+     @endif
+@endforeach
+
 </ul>
